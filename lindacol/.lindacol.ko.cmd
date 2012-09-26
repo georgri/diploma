c310d264e7a2a829c09dd9f85bf50b28d7e2fe2a
Georgy Riskov <georgris@gmail.com>
2012-09-27 01:07:30 +0400

    total erase

--- lindacol/.lindacol.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/georgri/coding/lindacol/lindacol/lindacol.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.2.0-23-generic/scripts/module-common.lds --build-id  -o /home/georgri/coding/lindacol/lindacol/lindacol.ko /home/georgri/coding/lindacol/lindacol/lindacol.o /home/georgri/coding/lindacol/lindacol/lindacol.mod.o
--- run.sh
+++
@@ -1,3 +0,0 @@
-sudo mount Downloads
-sudo rmmod lindacol;
-./test.sh && sudo insmod ./lindacol/lindacol.ko && sudo cat /sys/kernel/debug/lindacol/output
